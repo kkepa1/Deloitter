3ca705b022bb2182da421c58eb4fa0b6d8794570
Lukasz Mowinski <lmowinski@deloittece.com>
2022-08-01 13:48:39 +0200

    initial commit

---
+++ src/main/java/com/deloitte/deloitter/DeloitterApplication.java
@@ -0,0 +1,13 @@
+package com.deloitte.deloitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeloitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeloitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deloitte/deloitter/DeloitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deloitte.deloitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeloitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
