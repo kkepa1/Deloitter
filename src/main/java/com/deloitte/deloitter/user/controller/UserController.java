7c2b66cbfd8c981813791b9f3c1f9fc7bb7c755a
Lukasz Mowinski <lmowinski@deloittece.com>
2022-08-03 13:51:21 +0200

    added Dwitt package

--- src/main/java/com/deloitte/deloitter/user/controller/UserController.java
+++ src/main/java/com/deloitte/deloitter/user/controller/UserController.java
@@ -4,8 +4,8 @@ package com.deloitte.deloitter.user.controller;
 import com.deloitte.deloitter.user.entity.User;
 import com.deloitte.deloitter.user.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 
 @RestController
@@ -16,13 +16,19 @@ public class UserController {
     IUserService userService;
 
     @GetMapping(value = "/{name}")
-    public User getUser(@PathVariable String name) {return userService.findUserByName(name);}
+    public User getUser(@PathVariable String name) {
+        return userService.findUserByName(name);
+    }
 
     @PostMapping(value = "")
-    public User saveUser(@Valid @RequestBody User user) {return userService.saveUser(user);}
+    public User saveUser(@Valid @RequestBody User user) {
+        return userService.saveUser(user);
+    }
 
     @PutMapping(value = "")
-    public User updateUser(@Valid @RequestBody User user) {return userService.updateUser(user);}
+    public User updateUser(@Valid @RequestBody User user) {
+        return userService.updateUser(user);
+    }
 
     @DeleteMapping(value = "/{name}")
     public void deleteUser(@PathVariable String name) {
